7560b974f69b6d2b682890c9317806383ad52eac
danmartin11708 <danieljoseph11708@gmail.com>
2024-09-21 13:06:58 +0530

    commiting third code

---
+++ src/org/javascripts/JavaScriptExecutor.java
@@ -0,0 +1,15 @@
+package org.javascripts;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.decorators.WebDriverDecorator;
+
+public class JavaScriptExecutor {
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+	
+		
+	}
+
+}
---
+++ src/org/javascripts/WebDriver.java
@@ -0,0 +1,5 @@
+package org.javascripts;
+
+public interface WebDriver {
+
+}
